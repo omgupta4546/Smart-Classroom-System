37effb4adf43a7aef62b8ea6fa8b03b1bd7c38cf
Pankaj Sharma <shadowchaser9910@gmail.com>
2026-02-06 01:22:34 +0530

    Fix_Routing_HashRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
@@ -10,11 +10,11 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
-            <BrowserRouter>
+            <HashRouter>
                 <AuthProvider>
                     <App />
                 </AuthProvider>
-            </BrowserRouter>
+            </HashRouter>
         </GoogleOAuthProvider>
     </React.StrictMode>,
 )
